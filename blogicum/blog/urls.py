4b6652130ddaaabe92645687b065a1c8b87db541
Екатерина Пархачева <Parkhacheva.05@mail.ru>
2025-05-06 23:27:46 +0300

    CBV

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('creat/', views.post, name='creat_post'),
-    path('<int:id>/edit/', views.post, name='edit_post'),
-    path('<int:id>/delete/', views.delete_post, name='delete_post'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/<int:id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('creat/', views.PostCreateView.as_view(), name='creat_post'),
+    path('<int:id>/edit/', views.PostUpdateView.as_view(), name='edit_post'),
+    path('<int:id>/delete/', views.PostDeleteView.as_view(), name='delete_post'),
+    path('category/<slug:category_slug>/', views.CategoryPostsView.as_view(), name='category_posts'),
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,81 +4,84 @@ from .models import Post, Category
 from .forms import PostForm
 from django.urls import reverse
 from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
 
 
-def post(request, id=None):
-    if id is not None:
-        instance = get_object_or_404(Post, id=id)
-    else:
-        instance = None
-    form = PostForm(request.POST or None, files=request.FILES or None, instance=instance)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-    return render(request, 'blog/creat.html', context)
+class PostMixin:
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/creat.html'
+    success_url = reverse_lazy('blog:index')
 
 
-def delete_post(request, id):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('blog:index')
-    return render(request, 'blog/creat.html', context)
+class PostCreateView(PostMixin, CreateView):
+    pass
 
 
-def index(request):
-    template = 'blog/index.html'
-    current_datetime = datetime.now()
-    posts = Post.objects.filter(
-        pub_date__lte=current_datetime,  # __lte - меньше или равно
-        is_published=True,
-        category__is_published=True
-    ).order_by('-pub_date')
-
-    paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
-    return render(request, template, context)
+class PostUpdateView(PostMixin, UpdateView):
+    pass
 
 
-def post_detail(request, id):
-    template = 'blog/detail.html'
-    current_datetime = datetime.now()
-    post = get_object_or_404(
-        Post,
-        id=id,
-        is_published=True,
-        pub_date__lte=current_datetime,
-        category__is_published=True
-    )
-
-    context = {'post': post}
-    return render(request, template, context)
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/creat.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('blog:index')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем полный объект поста в контекст
+        context['post'] = self.get_object()
+        return context
 
 
-def category_posts(request, category_slug):
-    template = 'blog/category.html'
-    current_datetime = datetime.now()
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pub_date'
+    paginate_by = 10
 
-    category = get_object_or_404(
-        Category,
-        slug=category_slug,
-        is_published=True
-    )
 
-    post_list = category.posts.filter(
-        pub_date__lte=current_datetime,
-        is_published=True
-    ).order_by('-pub_date')
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'id'  # Указываем, что параметр в URL называется 'id'
 
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        'category': category,
-        'page_obj': page_obj
-    }
-    return render(request, template, context)
+    def get_object(self, queryset=None):
+        current_datetime = datetime.now()  
+        return get_object_or_404(
+            Post,
+            id=self.kwargs['id'],  # Используем self.kwargs['id'] вместо id
+            is_published=True,
+            pub_date__lte=current_datetime,
+            category__is_published=True
+        )
+
+
+class CategoryPostsView(ListView):
+    template_name = 'blog/category.html'
+    context_object_name = 'page_obj'
+    paginate_by = 10
+    ordering = '-pub_date'
+
+    def get_queryset(self):
+        # Получаем категорию и сохраняем в атрибуте класса
+        self.category = get_object_or_404(
+            Category,
+            slug=self.kwargs['category_slug'],
+            is_published=True
+        )
+        current_datetime = datetime.now()
+        # Возвращаем отфильтрованные посты
+        return self.category.posts.filter(
+            pub_date__lte=current_datetime,
+            is_published=True
+        ).order_by(self.ordering)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавляем категорию в контекст
+        context['category'] = self.category
+        return context
